53ed626a3a30df35b07248a2b3b57640467cd0e7
Jatin Lodhi <jatinlodhi111@gmail.com>
2024-12-29 19:39:39 +0530

    added new fe url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://second-brain-35rwdyjzs-jatinmes-projects.vercel.app/",
+  base: "https://second-brain-fe-sigma.vercel.app/",
  });
